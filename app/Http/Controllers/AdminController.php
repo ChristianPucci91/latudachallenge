2dd2c105137e9eb20e6f8c3d797d40c77db73832
ChristianPucci91 <pucci.christian91@gmail.com>
2021-08-11 12:33:37 +0200

    removing admin middleware

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,10 +10,10 @@ use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
-  public function __construct()
-  {
-      $this->middleware('admin');
-  }
+  // public function __construct()
+  // {
+  //     $this->middleware('admin');
+  // }
 
   public function index() {
     return view('admin.index');
--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::middleware('auth')->group(function(){
 
 });
 
-Route::prefix('/admin')->name('admin.')->middleware(['admin','auth'])->group(function()
+Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function()
 {
    //routes prefix with admin
      Route::get('/dashboard', 'AdminController@index')->name('index');
