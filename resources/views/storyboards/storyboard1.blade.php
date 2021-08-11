71f1cd7e7a3754fdf85a5aa27d2ca2d79c4a5bd4
ChristianPucci91 <pucci.christian91@gmail.com>
2021-08-11 13:09:51 +0200

    scenario fixing

--- resources/views/storyboards/storyboard1.blade.php
+++ resources/views/storyboards/storyboard1.blade.php
@@ -18,20 +18,18 @@
 }
 </style>
 <h1>Storyboard1</h1>
-<div class="embed-responsive embed-responsive-4by3x">
-  <iframe class="embed-responsive-item"src="{{asset('storage/storyboard1/story_html5.html')}}" class="video" width="" height=""></iframe>
-</div>
-{{-- <div class="containter">
+
+ <div class="containter">
 
   <div class="row justify-content-center mb-4" style="margin:0;">
-  <div class="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-8">
+  <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
     <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" src="https://vimeo.com/event/1041042/embed/9c1675cfa1?autoplay=1" title="Vimeo video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      {{-- <iframe class="embed-responsive-item" src="https://vimeo.com/event/1041042/embed/9c1675cfa1?autoplay=1" title="Vimeo video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> --}}
       <iframe  src="{{asset('storage/storyboard1/story_html5.html')}}" class="video" width="" height=""></iframe>
 
     </div>
   </div>
 </div>
-</div> --}}
+</div>
 @include('footer.footer')
 @endsection
